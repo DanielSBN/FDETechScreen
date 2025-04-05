8073d307b0377a6052254afafb6fc631548d08c6
Daniel Sanchez Buitrago <dansanchezbui@unal.edu.co>
2025-04-05 15:43:19 -0500

    Added negative number check

--- sort.py
+++ sort.py
@@ -1,26 +1,31 @@
 import sys
 
-ERROR_MESSAGE = 'ERROR: all values must be numeric'
+NUMERIC_ERROR_MESSAGE = 'ERROR: all values must be numeric'
 
 def sort(width: float, height: float, length: float, mass: float) -> str:
+    if width <= 0 or height <= 0 or length <= 0 or mass <= 0:
+        print('ERROR: all values must be positive')
+        return None
+
     try:
         is_heavy = mass >= 20
         is_bulky = width >= 150 or height >= 150 or length >= 150 or width * height * length >= 1000000
     except TypeError:
-        print(ERROR_MESSAGE)
+        print(NUMERIC_ERROR_MESSAGE)
         return None
 
     if is_bulky and is_heavy:
         return 'REJECTED'
     
     if is_bulky or is_heavy:
-        print('HERE')
         return 'SPECIAL'
     
     return 'STANDARD'
 
 if __name__ == '__main__':
     try:
-        print('The package was sorted into the following stack:', sort(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4])))
+        stack = sort(float(sys.argv[1]), float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4]))
+        if stack is not None:
+            print('The package was sorted into the following stack:', stack)
     except ValueError:
-        print(ERROR_MESSAGE)
+        print(NUMERIC_ERROR_MESSAGE)
